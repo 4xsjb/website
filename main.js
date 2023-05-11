9e097708b7f2b76b9e4af1726231095ece3c59d0
4xsjb <124498559+4xsjb@users.noreply.github.com>
2023-05-11 21:11:01 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -9,13 +9,13 @@ let closebtn = document.getElementById("close");
  
 firstbtn.addEventListener('click', function(){
     firstbtn.style.display = "none";
-    navbar.style.display = "flex";
+    navbar.style.transform = "translateX(0%)";
     closebtn.classList.remove('hide');
 })
 
 closebtn.addEventListener('click', function() {
     closebtn.classList.add('hide');
-    navbar.style.display= "none";
+    navbar.style.transform = "translateX(-100%)"; 
     firstbtn.style.display = "inline";
 })
 
